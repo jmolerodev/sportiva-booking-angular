bfe4379f039e955cb2509b4bb8ae23bf0ee8c7f1
jmolmardev <jmolero0896@gmail.com>
2026-02-04 17:02:21 +0100

    Despliegue inicial del proyecto

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAf3q8q6ulCvhomjWiriXZDtLY6naVw0Cs",
+    authDomain: "sportivabooking.firebaseapp.com",
+    projectId: "sportivabooking",
+    storageBucket: "sportivabooking.firebasestorage.app",
+    messagingSenderId: "969230804739",
+    appId: "1:969230804739:web:7b66c567990c194a2d2b36"
+  }
+};
